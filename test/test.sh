37fc43b984ec9bade9da1c9a1b57105ffc7b0a69
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2022-11-05 20:01:43 +0000

    Basic tests working

--- test/test.sh
+++ test/test.sh
@@ -2,30 +2,25 @@
 #
 # Test binary by running various commands
 #
-# 
-# abs_path_to_enclosing_dir () {
-#    echo "$(dirname $(cd $(dirname "$1");pwd)/$(basename "$1"))"
-# }
-# 
-# TEST_ROOT=$(abs_path_to_enclosing_dir $0)
-# BIN_PATH="products/ocr"
-#
-# EXPECTED_OUTPUT=""
-#
-# test_ocr_file() {
-#     OUTPUT=$("$BIN_PATH" -i "$1" -d)
-#
-#     if [ "$OUTPUT" != "$EXPECTED_OUTPUT" ]; then
-#         echo "Unexpected output: ${OUTPUT}"
-#         exit 1
-#     fi
-# }
 
-# WAV_PATH="$TEST_ROOT/test.wav"
-# test_transcribe_file $WAV_PATH
-#
-# WAV_PATH="$TEST_ROOT/test.mp3"
-# test_transcribe_file $WAV_PATH
-#
-# cat "$WAV_PATH" | "$BIN_PATH" -i '-' -f 'wav'
-# cat "$WAV_PATH" | "$BIN_PATH" -i '-' -f 'mp3'
+abs_path_to_enclosing_dir () {
+   echo "$(dirname $(cd $(dirname "$1");pwd)/$(basename "$1"))"
+}
+
+TEST_ROOT=$(abs_path_to_enclosing_dir $0)
+BIN_PATH="products/ocr"
+
+test_ocr_file() {
+    OUTPUT=$("$BIN_PATH" "$1")
+
+    if [ "$OUTPUT" != "$2" ]; then
+        echo "Unexpected output: ${OUTPUT}"
+        exit 1
+    fi
+}
+
+EN_PNG_PATH="$TEST_ROOT/english.png"
+test_ocr_file $EN_PNG_PATH "The rain in Spain stays mainly in the plain."
+
+# FR_PNG_PATH="$TEST_ROOT/french.png"
+# test_ocr_file $FR_PNG_PATH
