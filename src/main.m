71bce9bf3836c063f8e8f2410917947e8c702cbd
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2022-11-06 15:47:29 +0000

    Getopt parsing working

--- src/main.m
+++ src/main.m
@@ -33,6 +33,7 @@
 #import <Cocoa/Cocoa.h>
 #import <getopt.h>
 
+#import "Common.h"
 #import "BKSTextPiece.h"
 #import "BKSOCRBoss.h"
 
@@ -67,74 +68,174 @@ static NSComparisonResult byStartX(id  _Nonnull obj1, id  _Nonnull obj2, void *u
     return NSOrderedSame;
 }
 
+static BOOL IsRightOSVersion(void);
+static void PrintVersion(void);
+static void PrintHelp(void);
+static void ocr(NSString *path);
+
+static const char optstring[] = "l:hv";
+
+static struct option long_options[] = {
+    
+    // Specify language (locale) for STT
+    {"language",                  required_argument,  0, 'l'},
+    
+    {"help",                      no_argument,        0, 'h'},
+    {"version",                   no_argument,        0, 'v'},
+    
+    {0,                           0,                  0,  0 }
+};
+
+
+
 // A command line tool that given the path to an image file
 // writes the array of text pieces to the standard output as a plist.
-int main(int argc, const char * argv[]) {
-  @autoreleasepool {
-    if (@available(macOS 10.15, *)) {
-    } else {
-      fprintf(stderr, "Required macOS 10.15 or newer.\n");
-      return 1;
-    }
-    if (argc == 2) {
-      BKSOCRBoss *boss = [[BKSOCRBoss alloc] init];
-      NSFileManager *fm = [NSFileManager defaultManager];
-      NSString *path = [fm stringWithFileSystemRepresentation:argv[1] length:strlen(argv[1])];
-      NSURL *url = [NSURL fileURLWithPath:path];
-      if (nil == url) {
-          fprintf(stderr, "Not found:%s.\n", argv[1]);
-          return 1;
-      }
-      NSImage *image = [[NSImage alloc] initWithContentsOfURL:url];
-      if (nil == image) {
-          fprintf(stderr, "Not an image:%s.\n", argv[1]);
-          return 1;
-      }
-      NSError *error = nil;
-      NSArray<BKSTextPiece *> *pieces = [boss recognizeImageURL:url error:&error];
-      if (pieces) {
-        NSMutableArray<BKSTextPiece *> *sortPieces = [pieces mutableCopy];
-        // Sort by bottomRight.x to find the median.
-        [sortPieces sortUsingFunction:byEndX context:NULL];
-        CGFloat medianEndX = sortPieces[sortPieces.count/2].bottomRight.x;
-        [sortPieces sortUsingFunction:byStartX context:NULL];
-        CGFloat medianStartX = sortPieces[sortPieces.count/2].bottomLeft.x;
-        // Concatenate the page into an array of strings. If a previous line is short, the current one starts a new paragraph.
-        NSMutableArray *a = [NSMutableArray array];
-        for (NSUInteger i = 0; i < pieces.count; ++i) {
-          BKSTextPiece *piece = pieces[i];
-          if (piece.text) {
-            // If the current line is indented, adjust the previous separator to be a paragaph separator.
-            if (2 < a.count && medianStartX*1.1 < piece.bottomLeft.x && !hasInitialLowercase(piece.text)) {
-              a[a.count - 1] = @"\n";
-              [a addObject:piece.text];
-              // Insert a paragraph separator if this is a short line, or at the end.
-              if (piece.bottomRight.x < medianEndX*0.9 || i+1 == pieces.count){
-                [a addObject:@"\n"];
-              }else {
-                [a addObject:@" "];
-              }
-            } else if (2 < a.count && [@" " isEqual:a.lastObject] && [a[a.count - 2] hasSuffix:@"-"]) {
-              // If this isn't the first line of a paragraph, and the previous line ends in '-' assume
-              // it is hypenated, and delete the hyphen and join.
 
-              NSString *lastLine = a[a.count - 2];
-              a[a.count - 2] = [[lastLine substringToIndex:lastLine.length-1] stringByAppendingString:piece.text];
-            } else {
-              [a addObject:piece.text];
-              // Insert a paragraph separator if this is a short line, or at the end.
-              if (piece.bottomRight.x < medianEndX*0.9 || i+1 == pieces.count){
-                [a addObject:@"\n"];
-              }else {
-                [a addObject:@" "];
-              }
+int main(int argc, const char * argv[]) {
+    // Make sure we're running on a macOS version that supports speech recognition
+    if (IsRightOSVersion() == NO) {
+        NSPrintErr(@"This program requires macOS 10.15 or later.");
+        exit(EXIT_FAILURE);
+    }
+    
+    // Parse arguments
+    int optch;
+    int long_index = 0;
+    
+    while ((optch = getopt_long(argc, (char *const *)argv, optstring, long_options, &long_index)) != -1) {
+        switch (optch) {
+            
+            // Set language (i.e. locale) for speech recognition
+            case 'l':
+                //language = @(optarg);
+                break;
+            
+            // Print version
+            case 'v':
+                PrintVersion();
+                exit(EXIT_SUCCESS);
+                break;
+            
+            // Print help text with list of option flags
+            case 'h':
+            default:
+                PrintHelp();
+                exit(EXIT_SUCCESS);
+                break;
+        }
+    }
+    
+    // We always need one or more additional arguments, which should be paths to images
+    if (argc - optind < 1) {
+        NSPrintErr(@"Error: Missing argument.");
+        PrintHelp();
+        exit(EXIT_FAILURE);
+    }
+    
+    // Read remaining args as paths
+    NSMutableArray *imageFiles = [NSMutableArray array];
+    NSFileManager *fm = [NSFileManager defaultManager];
+    while (optind < argc) {
+        NSString *path = [fm stringWithFileSystemRepresentation:argv[optind]
+                                                         length:strlen(argv[optind])];
+        [imageFiles addObject:path];
+        optind += 1;
+    }
+    
+    //NSPrint(imageFiles[0]);
+    ocr(imageFiles[0]);
+    
+    return EXIT_SUCCESS;
+}
+    
+void ocr(NSString *path) {
+    BKSOCRBoss *boss = [[BKSOCRBoss alloc] init];
+    
+    NSURL *url = [NSURL fileURLWithPath:path];
+    if (nil == url) {
+        NSPrintErr(@"Not found: %@", path);
+        return;
+    }
+    
+    NSImage *image = [[NSImage alloc] initWithContentsOfURL:url];
+    if (nil == image) {
+        NSPrintErr(@"Not an image: %@", path);
+        return;
+    }
+    
+    NSError *error = nil;
+    NSArray<BKSTextPiece *> *pieces = [boss recognizeImageURL:url error:&error];
+    if (pieces) {
+      NSMutableArray<BKSTextPiece *> *sortPieces = [pieces mutableCopy];
+      // Sort by bottomRight.x to find the median.
+      [sortPieces sortUsingFunction:byEndX context:NULL];
+      CGFloat medianEndX = sortPieces[sortPieces.count/2].bottomRight.x;
+      [sortPieces sortUsingFunction:byStartX context:NULL];
+      CGFloat medianStartX = sortPieces[sortPieces.count/2].bottomLeft.x;
+      // Concatenate the page into an array of strings. If a previous line is short, the current one starts a new paragraph.
+      NSMutableArray *a = [NSMutableArray array];
+      for (NSUInteger i = 0; i < pieces.count; ++i) {
+        BKSTextPiece *piece = pieces[i];
+        if (piece.text) {
+          // If the current line is indented, adjust the previous separator to be a paragaph separator.
+          if (2 < a.count && medianStartX*1.1 < piece.bottomLeft.x && !hasInitialLowercase(piece.text)) {
+            a[a.count - 1] = @"\n";
+            [a addObject:piece.text];
+            // Insert a paragraph separator if this is a short line, or at the end.
+            if (piece.bottomRight.x < medianEndX*0.9 || i+1 == pieces.count){
+              [a addObject:@"\n"];
+            }else {
+              [a addObject:@" "];
+            }
+          } else if (2 < a.count && [@" " isEqual:a.lastObject] && [a[a.count - 2] hasSuffix:@"-"]) {
+            // If this isn't the first line of a paragraph, and the previous line ends in '-' assume
+            // it is hypenated, and delete the hyphen and join.
+
+            NSString *lastLine = a[a.count - 2];
+            a[a.count - 2] = [[lastLine substringToIndex:lastLine.length-1] stringByAppendingString:piece.text];
+          } else {
+            [a addObject:piece.text];
+            // Insert a paragraph separator if this is a short line, or at the end.
+            if (piece.bottomRight.x < medianEndX*0.9 || i+1 == pieces.count){
+              [a addObject:@"\n"];
+            }else {
+              [a addObject:@" "];
             }
           }
         }
-        NSString *all = [a componentsJoinedByString:@""];
-        printf("%s", [all UTF8String]);
       }
+      NSString *all = [a componentsJoinedByString:@""];
+      printf("%s", [all UTF8String]);
     }
   }
-  return 0;
+
+
+#pragma mark -
+
+static BOOL IsRightOSVersion() {
+    // The OCR API wasn't introduced until macOS 10.15
+    NSOperatingSystemVersion osVersion = {0};
+    osVersion.majorVersion = 10;
+    osVersion.minorVersion = 15;
+    return [[NSProcessInfo processInfo] isOperatingSystemAtLeastVersion:osVersion];
 }
+
+static void PrintVersion(void) {
+    NSPrint(@"%@ version %@ by %@", PROGRAM_NAME, PROGRAM_VERSION, PROGRAM_AUTHOR);
+}
+
+static void PrintHelp(void) {
+    PrintVersion();
+    NSPrint(@"\n\
+%@ [-s] [-l lang] file ...\n\
+\n\
+Options:\n\
+\n\
+    -l --language           Specify speech recognition language\n\
+\n\
+    -h --help               Prints help\n\
+    -v --version            Prints program name and version\n\
+\n\
+For further details, see 'man %@'.", PROGRAM_NAME, PROGRAM_NAME);
+}
+
